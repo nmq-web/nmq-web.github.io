3043c354a45cd6bbe3a7f3ca32029455afb5d0af
nmq-web <leoalexander.nmq@gmail.com>
2024-08-08 16:55:24 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// Navigation
+function openMenu() {
+    document.getElementsByClassName("navigation-4")[0].style.display = "block"
+    document.getElementsByClassName("navigation-5")[0].style.display = "block"
+}
+
+function closeMenu() {
+    document.getElementsByClassName("navigation-4")[0].style.display = "none"
+    document.getElementsByClassName("navigation-5")[0].style.display = "none"
+}
+
+// Back To Top
+function openBacktotop() {
+    document.getElementsByClassName("back-to-top")[0].style.display = "block"
+    document.getElementsByClassName("back-to-top-open")[0].style.display = "none"
+}
+
+function closeBacktotop() {
+    document.getElementsByClassName("back-to-top")[0].style.display = "none"
+    document.getElementsByClassName("back-to-top-open")[0].style.display = "block"
+
+}
